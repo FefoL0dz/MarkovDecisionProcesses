f8df9239d41dc038d5fa225563d371a4ae1e554b
flodes <felipe.lodes@meseems.com.br>
2020-07-12 18:29:46 -0300

    create packages

---
+++ src/mdp/iterationApproach/policy/Main.java
@@ -0,0 +1,9 @@
+package mdp.iterationApproach.policy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("TESTANDO 1, 2, 3... SOM, SOM...");
+    }
+}
