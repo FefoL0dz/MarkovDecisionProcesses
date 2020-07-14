4c14fde8201138d175e29be91c550052c9903ed0
hortencialopes <hortencialopes@gmail.com>
2020-07-13 23:19:52 -0300

    Leitura no Main MDP.ITERATION

--- src/mdp/iterationApproach/value/Main.java
+++ src/mdp/iterationApproach/value/Main.java
@@ -20,33 +20,38 @@ public class Main {
             String linha = lerArq.readLine(); // lê a primeira linha
             String REGEX = "(\\B\\d*)";
             Pattern posestado = Pattern.compile(REGEX);
-            float Estados[][];
+            float Estados[][] = new float [20][2];
             int max = -1;
+            int r = -1;
+            int c = -1;
             Matcher s;
+
             if(linha.equals("states")) {
                 while (!linha.equals("endstates")) {
                     s = posestado.matcher(linha);
                     while (s.find()){
                         if(!s.group(1).isEmpty()) {
-
-                            System.out.println(s.group(0) + " - ");
                             if (max < Integer.parseInt(s.group(1))) max = Integer.parseInt(s.group(1));
                             System.out.println("Max so far " + max);
                         }
                     }
-
                     linha = lerArq.readLine();
                 }
             }
+
             /* Matriz de estados criada */
-            
+            List<String> posicoes = new LinkedList<String>();
             Estados = new float[max][max];
 
-            linha = lerArq.readLine();
+            arq = new FileReader("C:\\Users\\horte\\IdeaProjects\\MarkovDecisionProcesses\\src\\mdp\\iterationApproach\\value\\navigation_1.txt");
+            lerArq = new BufferedReader(arq);
 
+            linha = lerArq.readLine(); // lê a primeira linha
+
+            linha = lerArq.readLine();
+            System.out.printf("%s\n", linha);
             while (linha != null) {
                 //System.out.printf("%s\n", linha);
-
                 linha = lerArq.readLine(); // lê da segunda até a última linha
             }
 
